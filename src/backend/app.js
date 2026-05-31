4676fa9196db593af6bd1470b96052040d02b519
Ale <a.cristoforetti-1@studenti.unitn.it>
2026-05-31 14:44:06 +0200

    Fixed API documentation

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const connectDB = require('../config/db');
+const cors = require('cors');
 
 const authRouter = require('./api/auth');
 const volunteerRouter = require('./api/volunteer');
@@ -14,6 +15,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Connessione asincrona al Database
 connectDB()
